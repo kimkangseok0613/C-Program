586554b5251046b0d92f293fcd3defe8022c59b7
kimkangseok0613 <ks0006131@gmail.com>
2026-02-13 21:42:18 +0900

    (2026.12.13) 얕은 복사 & 깊은 복사

---
+++ Class 03th (Constructor & Destructor)/Source.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+
+using namespace std;
+
+class Soldier
+{
+private:
+	static int count;
+
+public:
+	Soldier()
+	{
+		cout << "Created Soldier" << endl;
+	}
+	~Soldier()
+	{
+		count++;
+		cout << "Destroyed Count : " << count << endl;
+	}
+};
+
+int Soldier::count = 0;
+
+class Sprite // 얕은 복사
+{
+private:
+	int* pixels = nullptr;
+public:
+	Sprite()
+	{
+		if (pixels == nullptr)
+		{
+			pixels = new int(1024);
+		}
+	}
+
+	Sprite(const Sprite& clone)
+	{
+		pixels = clone.pixels;
+	}
+	~Sprite()
+	{
+		delete pixels;
+	}
+};
+
+class Mesh // 깊은 복사
+{
+private:
+	float* vertices = nullptr;
+public:
+	Mesh()
+	{
+		if (vertices == nullptr)
+		{
+			vertices = new float(512.0f);
+		}
+	}
+	Mesh(const Mesh& clone)
+	{
+		vertices = new float;
+
+		*vertices = *clone.vertices;
+	}
+	~Mesh()
+	{
+		delete vertices;
+	}
+};
+
+int main()
+{
+#pragma region 생성자
+	// 클래스의 인스턴스가 생성되는 시점에 자동으로
+	// 호출되는 특수한 멤버 함수입니다.
+
+	// Soldier soldier;
+
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출되며,
+	// 생성자는 반환형이 존재하지 않기 때문에 생성자가 호출되기
+	// 전에는 객체에 대한 메모리가 할당되지 않습니다.
+
+#pragma endregion
+
+#pragma region 소멸자
+	// 객체가 소멸될 때 자동으로 실행되는 클래스의
+	// 특수한 멤버 함수입니다.
+
+	// Soldier* pointer = new Soldier;
+	// 
+	// delete pointer;
+	// 
+	// pointer = new Soldier;
+	// 
+	// delete pointer;
+
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번만
+	// 호출되며, 소멸자에는 매개 변수를 생성하여 사용할
+	// 수 없습니다.
+
+#pragma endregion
+
+#pragma region 얕은 복사
+	// 객체를 복사할 때 주소 값을 복사하여 같은
+	// 메모리 공간을 가리키게 하는 복사입니다.
+
+	// int* address = new int;
+	// 
+	// int* share = address;
+	// 
+	// *address = 10;
+	// 
+	// cout << "address 변수의 값 : " << address << endl;
+	// cout << "share 변수의 값 : " << share << endl;
+	// 
+	// cout << "address가 가리키는 값 : " << *address << endl;
+	// cout << "share가 가리키는 값 : " << *share << endl;
+	// 
+	// delete address;
+
+	//Sprite original;
+	//
+	//Sprite copied(original);
+
+	// 얕은 복사의 경우 같은 객체가 서로 같은 메모리 공간을
+	// 참조하고 있기 때문에 하나의 객체로 값을 변경하게 되면
+	// 서로 참조된 객체도 함께 영향을 받습니다.
+#pragma endregion
+
+#pragma region 깊은 복사
+	// 객체를 복사할 때, 참조 값이 아닌 객체 자체로 새로
+	// 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+
+	// Mesh mesh;
+	// Mesh duplicate(mesh);
+
+#pragma endregion
+
+	return 0;
+}
