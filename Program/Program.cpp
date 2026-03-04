6eb8e4a7d33405c3950264d481dcff5ec95b676f
kimkangseok0613 <ks0006131@gmail.com>
2026-03-04 21:44:16 +0900

    (2026.03.04) 예외 처리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,22 +1,11 @@
 #include "stdafx.h"
+#include "Container.h"
 
 template <typename T>
 bool Same(T left,T right)
 {
 	return left == right;
 }
-
-template <typename T>
-class Container
-{
-	int size;
-	int index;
-public:
-	Container()
-	{
-
-	}
-};
 
 int main()
 {
@@ -25,12 +14,23 @@ int main()
 	// 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수 
 	// 있는 기능입니다.
 
-	cout << Same('A', 'A') << endl;
-	cout << Same(5, 10) << endl;
-	cout << Same(17.25f, 19.675f) << endl;
-	cout << Same('Head', 'Head') << endl;
+	// cout << Same('A', 'A') << endl;
+	// cout << Same(5, 10) << endl;
+	// cout << Same(17.25f, 19.675f) << endl;
+	// cout << Same('Head', 'Head') << endl;
+	// 
+	// Container<int> container(5);
 
 #pragma endregion
+
+#pragma region 템플릿 특수화
+	// 특정 자료형에 대해 다르게 처리하고 싶은 경우
+	// 특정한 자료형만 다른 형식으로 동작시키는 기능입니다.
+
+
+
+#pragma endregion
+
 
 	return 0;
 }
