cabde324c218eed5434fa9158d309bcce6b5f211
kimkangseok0613 <ks0006131@gmail.com>
2026-03-10 21:42:59 +0900

    (2026.03.10) 연관 컨테이너

---
+++ Class 11th (Container Adapter)/Source.cpp
@@ -0,0 +1,78 @@
+﻿#include "../Program/stdafx.h"
+#include <stack>
+#include <queue>
+
+int main()
+{
+#pragma region 컨테이너 어댑터
+	// 기존 컨테이너의 인터페이스를 제한하여 만든 
+	// 기능이 제한되거나 변경된 컨테이너입니다.
+
+#pragma region stack container
+
+	// stack<int> stack;
+	// 
+	// stack.push(10);
+	// stack.push(20);
+	// stack.push(30);
+	// stack.push(40);
+	// stack.push(50);
+	// 
+	// while (stack.empty() == false) //for (int i = 0; i < 5; i++)
+	// {
+	// 	cout << stack.top() << endl;
+	// 
+	// 	stack.pop();
+	// }
+
+#pragma endregion
+
+#pragma region queue container
+
+	// queue<int> queue;
+	// 
+	// queue.push(10);
+	// queue.push(20);
+	// queue.push(30);
+	// queue.push(40);
+	// queue.push(50);
+	// 
+	// const int& size = queue.size(); // int a= queue.size();
+	// 
+	// for (int i = 0; i < size; i++) // for (int i = 0; i < a; i++)
+	// {
+	// 	cout << queue.front() << endl;
+	// 
+	// 	queue.pop();
+	// }
+
+#pragma endregion
+
+#pragma region priority_queue
+	// 두 자식의 노드 중에서 값이 더 큰 쪽으로 교환합니다.
+	// 가장 아래에 자식 노드 중에서 가장 오른쪽에 
+	// 있는 노드를 최상위로 넣어줍니다.
+
+	// priority_queue<int> priority_queue;
+	// 
+	// priority_queue.push(30);
+	// priority_queue.push(50);
+	// priority_queue.push(40);
+	// priority_queue.push(10);
+	// priority_queue.push(60);
+	// priority_queue.push(20);
+	// 
+	// while (priority_queue.empty() == false)		// const int& size = priority_queue.size();
+	// {											// 
+	// 	cout << priority_queue.top() << endl;	// for (int i = 0; i < size; i++)
+	// 											// {
+	// 	priority_queue.pop();					// 	cout << priority_queue.top() << endl;
+	// }											// 	priority_queue.pop();
+												// }
+
+#pragma endregion
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,81 +1,11 @@
 #include "stdafx.h"
-#include <vector>
-#include <list>
-#include <deque>
 
 int main()
 {
-#pragma region 선형 컨테이너
-	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 컨테이너입니다.
+#pragma region 연관 컨테이너
+	// 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
 
-#pragma region vector container
 
-	// vector<int> vector;
-	// 
-	// vector.reserve(8);
-	// 
-	// vector.push_back(10);
-	// 
-	// vector.push_back(20);
-	// 
-	// vector.push_back(30);
-	// 
-	// vector.push_back(40);
-	// 
-	// vector.push_back(50);
-	// 
-	// vector.pop_back();
-	// 
-	// for (int i = 0; i < vector.size(); i++)
-	// {
-	// 	cout << vector[i] << endl;
-	// }
-	// 
-	// vector.clear();
-	// 
-	// cout << "vector Size : " << vector.size() << endl;
-	// cout << "vector Capacity : " << vector.capacity() << endl;
-
-#pragma endregion
-
-#pragma region list container
-	
-	// list<int> list;
-	// 
-	// list.push_back(20);
-	// list.push_back(30);
-	// list.push_front(10);
-	// 
-	// list.pop_front();
-	// 
-	// list.push_front(1);
-	// 
-	// list.remove(20);
-	// 
-	// for (const int& element : list)
-	// {
-	// 	cout << element << endl;
-	// }
-	// cout << "list size : " << list.size() <<"\n" << endl;
-	// 
-	// list.assign(3, 5); // size 3, 5로 채우기
-	// for (const int& element : list)
-	// {
-	// 	cout << element << endl;
-	// }
-	// 
-	// cout << "list size : " << list.size() << endl;
-
-#pragma endregion
-
-#pragma region deque container
-	// Double-Ended Queue
-
-	deque<int> deque;
-
-	
-
-#pragma endregion
 
 #pragma endregion
 
